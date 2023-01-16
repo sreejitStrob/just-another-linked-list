ef564e211d4b1033304d28091dda2366e66f3315
Sreejit <sreejit9821@gmail.com>
2023-01-16 20:53:05 +0530

    Linked list change

---
+++ LinkedList.js
@@ -0,0 +1,38 @@
+import LinkedListNode from "./LinkedListNode.js";
+
+export default class LinkedList {
+    constructor() {
+        this.head = null
+        this.length = 0
+    }
+
+    insertAtHead(value) {
+        const newNode = new LinkedListNode(value,this.head);
+    }
+
+
+    //insert at tail
+
+
+    //insert at index
+
+
+    //remove at index
+
+
+    //remove from head
+
+
+    //remove from tail
+
+
+    //print all
+
+
+    //print at index
+
+    //convert to array
+
+
+    
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+import LinkedList from "./LinkedList.js";
+
+const ll = new LinkedList();
+ll.insertAtHead(10);
+console.log(11);
